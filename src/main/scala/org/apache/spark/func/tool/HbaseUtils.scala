df6c4d6df3bee012398c23e34e38380b918a0b5b
LMQ <LMQ>
2018-04-10 09:39:20 +0800

    增加一个方法

--- src/main/scala/org/apache/spark/func/tool/HbaseUtils.scala
+++ src/main/scala/org/apache/spark/func/tool/HbaseUtils.scala
@@ -6,8 +6,16 @@ import org.apache.hadoop.hbase.mapreduce.TableOutputFormat
 
 trait HbaseUtils {
   def createJob(table: String, conf: Configuration): Configuration = {
+    val job = Job.getInstance(conf, this.getClass.getName.split('$')(0))
+    job.setOutputFormatClass(classOf[TableOutputFormat[String]])
+    job.getConfiguration
+  }
+  def createJob(
+      zk:String,
+      table: String, 
+      conf: Configuration): Configuration = {
     conf.set(TableOutputFormat.OUTPUT_TABLE, table)
-    conf.set("hbase.zookeeper.quorum ","zkNode1,zkNode2,zkNode3")
+    conf.set("hbase.zookeeper.quorum ",zk)
     conf.set("zookeeper.znode.parent","/hbase")
     val job = Job.getInstance(conf, this.getClass.getName.split('$')(0))
     job.setOutputFormatClass(classOf[TableOutputFormat[String]])
