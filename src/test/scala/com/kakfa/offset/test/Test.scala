ba9aa43ffa098ebae2a33f99713bf8197400fe0e
LMQ <LMQ>
2018-04-24 11:46:48 +0800

    test

--- src/test/scala/com/kakfa/offset/test/Test.scala
+++ src/test/scala/com/kakfa/offset/test/Test.scala
@@ -8,19 +8,27 @@ object Test {
   val smp = new SimpleDateFormat("yyyyMMdd")
   PropertyConfigurator.configure("conf/log4j.properties")
   val LOG=LoggerFactory.getLogger("Test")
-  val zk="192.168.0.235,192.168.0.231,192.168.0.234"
+  //val zk="192.168.0.235,192.168.0.231,192.168.0.234"
+  val zk="solr1,solr2,datanode37"
   val broker = "kafka1,kafka2,kafka3"
   val topics = Set("test","117--12")
   def main(args: Array[String]): Unit = {
     val groupid = "kafkadayoffset"
-    val day = "20180413"
-    val hour="18"
+    val day = "20180423"
+    val hour="11"
     var kafkaParams = Map[String, String](
       "metadata.broker.list" -> broker,
       "serializer.class" -> "kafka.serializer.StringEncoder",
       "group.id" -> groupid)
+    val statTime=new Date().getTime
     val kafkaoffsetUtil = KafkaOffsetUtil(kafkaParams, zk)
-    kafkaoffsetUtil.recordDayHourOffsetToZK(day,hour,null)
+    println(kafkaoffsetUtil.getAlltopics())
+    
+    /*val r=kafkaoffsetUtil.recordDayOffsetsToZK(day, topics)
+    if(r.isRight){
+      r.right.get.foreach(println)
+    }*/
+    //kafkaoffsetUtil.recordDayHourOffsetToZK(day,hour,null)
     /*val res = kafkaoffsetUtil.getDayHourOffsetsFromZK(day, hour,topics)
     if (res.isLeft) {
       LOG.info(res.left.get)
