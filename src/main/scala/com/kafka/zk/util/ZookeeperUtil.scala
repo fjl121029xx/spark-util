1006895248758064dd179c969a14b4d932a26230
LMQ <LMQ>
2018-04-24 11:46:43 +0800

    zookeeperUtil

--- src/main/scala/com/kafka/zk/util/ZookeeperUtil.scala
+++ src/main/scala/com/kafka/zk/util/ZookeeperUtil.scala
@@ -5,27 +5,30 @@ import kafka.utils.ZKStringSerializer
 import org.apache.zookeeper.CreateMode
 import org.slf4j.LoggerFactory
 
-class ZookeeperUtil(val zk:String) {
+class ZookeeperUtil(val zk: String) {
   val PERSISTENT = CreateMode.PERSISTENT //短暂，持久
   val EPHEMERAL = CreateMode.EPHEMERAL
-  var zkClient:ZkClient=getzkClient(zk)
-  lazy val LOG=LoggerFactory.getLogger("ZookeeperUtil")
+  var zkClient: ZkClient = getzkClient(zk)
+  lazy val LOG = LoggerFactory.getLogger("ZookeeperUtil")
   def getzkClient(zk: String) = {
-    if(zkClient==null){
-     zkClient = new ZkClient(zk, 10000, 10000, ZKStringSerializer)
+    if (zkClient == null) {
+      zkClient = new ZkClient(zk, 10000, 10000, ZKStringSerializer)
     }
     zkClient
   }
   def isExist(path: String) = {
     zkClient.exists(path)
   }
+  def deletePath(path: String) = {
+    zkClient.delete(path)
+  }
   /**
    * 功能：创建目录，如果不存在就创建。
    */
   def createFileOrDir(
     path: String,
-    data: String = ""):Either[String,Boolean] = {
-    if (!zkClient.exists(path)){
+    data: String = ""): Either[String, Boolean] = {
+    if (!zkClient.exists(path)) {
       try {
         zkClient.create(path, data, PERSISTENT)
         new Right(true)
@@ -33,9 +36,9 @@ class ZookeeperUtil(val zk:String) {
         case t: Throwable =>
           LOG.error("多级目录请使用 createMultistagePath 方法")
           LOG.error(t.toString())
-          new Left(t.toString()+"\n" + t.getStackTraceString)
+          new Left(t.toString() + "\n" + t.getStackTraceString)
       }
-    }else new Right(true)
+    } else new Right(true)
   }
   /**
    * 功能：创建多级目录
@@ -50,7 +53,7 @@ class ZookeeperUtil(val zk:String) {
       }
     }
   }
-  
+
   def readData(
     path: String) = {
     zkClient.readData(path).toString()
