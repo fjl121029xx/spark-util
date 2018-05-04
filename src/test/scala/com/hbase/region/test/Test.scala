591b5e3781bebcab1ad3061fab23e875fff9e85f
LMQ <LMQ>
2018-05-04 11:46:23 +0800

    splitregion

---
+++ src/test/scala/com/hbase/region/test/Test.scala
@@ -0,0 +1,21 @@
+package com.hbase.region.test
+
+import com.hbase.operat.handler.HbaseSplitAdmin
+import scala.collection.JavaConversions._
+object Test {
+  def main(args: Array[String]): Unit = {
+    val admin = new HbaseSplitAdmin("zk1,zk2,zk3")
+    admin.getTableRegionName("test").foreach { hr => 
+    println(hr.getRegionId,new String(hr.getStartKey),new String(hr.getEndKey))}
+    val a = admin.getAllTableregionInfo()
+    a.foreach {case (tablename, regionLoadlist) =>
+      if(tablename=="test"){
+        val regionNameAndSize = regionLoadlist
+        .map{x=>(x.getNameAsString,x.getStorefileSizeMB)}
+        //admin.splitRegion("regionName")
+        println(tablename,regionNameAndSize)
+      } 
+    }
+
+  }
+}
