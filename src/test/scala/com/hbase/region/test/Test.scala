2db64e416c9c2bceb84275fb3829016d22c48997
LMQ <LMQ>
2018-05-04 11:54:33 +0800

    test

--- src/test/scala/com/hbase/region/test/Test.scala
+++ src/test/scala/com/hbase/region/test/Test.scala
@@ -5,10 +5,14 @@ import scala.collection.JavaConversions._
 object Test {
   def main(args: Array[String]): Unit = {
     val admin = new HbaseSplitAdmin("zk1,zk2,zk3")
-    admin.getTableRegionName("test").foreach { hr => 
-    println(hr.getRegionId,new String(hr.getStartKey),new String(hr.getEndKey))}
-    val a = admin.getAllTableregionInfo()
-    a.foreach {case (tablename, regionLoadlist) =>
+    admin
+    .getTableRegionName("test")
+    .foreach { hr => 
+    println(hr.getRegionId,new String(hr.getStartKey),new String(hr.getEndKey))
+    }
+    admin
+    .getAllTableregionInfo()
+    .foreach {case (tablename, regionLoadlist) =>
       if(tablename=="test"){
         val regionNameAndSize = regionLoadlist
         .map{x=>(x.getNameAsString,x.getStorefileSizeMB)}
